9346e214f870ce0ba51e1f4f46cf54aa48336a00
Ailton Filho <ailtoncsf@yahoo.com.br>
2018-02-23 11:20:30 -0300

    Aplicando um middleware Query Parse Int.

--- my-money-app/backend/src/config/server.js
+++ my-money-app/backend/src/config/server.js
@@ -4,11 +4,13 @@ const bodyParser = require('body-parser')
 const express = require('express')
 const server = express()
 const allowCors = require('../config/cors')
+const queryParser = require('express-query-int')
 
 //usando o body parser para interpretar todas requisições 
 server.use(bodyParser.urlencoded({ extended: true }))
 server.use(bodyParser.json())
 server.use(allowCors)
+server.use(queryParser())
 
 server.listen(port, function() {
     console.log(`BACKEND is running on port ${port}.`)
