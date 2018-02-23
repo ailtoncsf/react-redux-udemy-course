35acdd997d1996f9edbf901efd609a959a79ab0a
Ailton Filho <ailtoncsf@yahoo.com.br>
2018-02-23 10:30:45 -0300

    Habilitando CORS para aplicação.

--- my-money-app/backend/src/config/server.js
+++ my-money-app/backend/src/config/server.js
@@ -3,10 +3,12 @@ const port = 3003
 const bodyParser = require('body-parser')
 const express = require('express')
 const server = express()
+const allowCors = require('../config/cors')
 
 //usando o body parser para interpretar todas requisições 
 server.use(bodyParser.urlencoded({ extended: true }))
 server.use(bodyParser.json())
+server.use(allowCors)
 
 server.listen(port, function() {
     console.log(`BACKEND is running on port ${port}.`)
