a805087b85070e6627ba45d4155e70ec7e87f76f
Ailton Filho <ailtoncsf@yahoo.com.br>
2018-03-29 15:24:58 -0300

    Criando billingCycleForm com Redux.

---
+++ my-money-app/frontend/src/billingCycle/billingCycleForm.jsx
@@ -0,0 +1,21 @@
+import React, { Component } from 'react'
+import { bindActionCreators } from 'redux'
+import { connect } from 'react-redux'
+
+
+class BillingCycleForm extends Component {
+    render() {
+        return (
+            <form role="form">
+                <div className="box-body">
+                
+                </div>
+                <div className="box-footer">
+                    <button type='submit' className="btn btn-primary">Submit</button>
+                </div>
+            </form>
+        )
+    }
+}
+
+export default BillingCycleForm
