e72adc54349b74dc581ec6afaa819e8a8d92ffb0
Ailton Filho <ailtoncsf@yahoo.com.br>
2018-04-05 08:46:38 -0300

    Integrando soma de créditos e débitos no formulário.

--- my-money-app/frontend/src/billingCycle/billingCycleForm.jsx
+++ my-money-app/frontend/src/billingCycle/billingCycleForm.jsx
@@ -10,15 +10,26 @@ import Summary from './summary'
 
 class BillingCycleForm extends Component {
 
+    calculateSummary() {
+        //implementando map reducer com javascript
+        const sum = (t, v) => t + v
+        return {
+            sumOfCredits: this.props.credits.map(cred => +cred.value || 0).reduce(sum),
+            sumOfDebts: this.props.debts.map(deb => +deb.value || 0).reduce(sum)
+        }
+    }
+
     render() {
         const { handleSubmit, readOnly, credits, debts } = this.props
+        const { sumOfCredits, sumOfDebts } = this.props.calculateSummary()
+
         return (
             <form role="form" onSubmit={handleSubmit}>
                 <div className="box-body">
                     <Field name='name' component={LabelAndInput} readOnly={readOnly} label='Nome' cols='12 4' placeholder='Informe o nome' />
                     <Field name='month' component={LabelAndInput} readOnly={readOnly} label='Mês' cols='12 4' placeholder='Informe o mês' />
                     <Field name='year' component={LabelAndInput} readOnly={readOnly} label='Ano' cols='12 4' placeholder='Informe o ano' />
-                    <Summary credit={1000} debt={100} />
+                    <Summary credit={sumOfCredits} debt={sumOfDebts} />
                     <ItemList cols='12 6' list={credits} readOnly={readOnly} field='credits' legend='Créditos' />
                     <ItemList cols='12 6' list={debts} readOnly={readOnly} field='debts' legend='Déditos' showStatus={true} />
                 </div>
